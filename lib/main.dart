b7f31a1d4079e1d8dfd7ef5f7171ea376d11e8a5
Berlinkg <nadyrbekov570@gmail.com>
2023-02-15 20:25:59 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:game_man/ui/color.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({super.key});
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.primaryColor,
+    );
+  }
+}
---
+++ lib/ui/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color primaryColor = Color(0xFF39299A);
+  static Color primaryColorDark = Color(0xFF231954);
+  // static Color primaryColor = Color(0xFF39299A);
+  //  static Color primaryColor = Color(0xFF39299A);
+  //   static Color primaryColor = Color(0xFF39299A);
+}
